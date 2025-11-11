aeb7ccbc44394ea231a63de30a12353f3d913837
shahneouj <shahneouj@gmail.com>
2025-11-11 15:37:35 +0600

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Movie Master Pro Server is running");
 });
-
+//mongodb
 const uri = process.env.MONGODB_URL;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -29,8 +29,22 @@ async function run() {
     // Connect the client to the server    (optional starting in v4.7)
     await client.connect();
 
+    //connect to the collection
     const db = client.db("bill_db");
+    const billCollection = db.collection("bills");
 
+    // all the routes
+    app.get("/bills", async (req, res) => {
+      try {
+        const query = {};
+        const cursor = billCollection.find(query);
+        const result = await cursor.toArray();
+        res.send(result);
+      } catch (error) {
+        console.error("Error fetching bills:", error);
+        res.status(500).send("Internal Server Error");
+      }
+    });
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -38,11 +52,10 @@ async function run() {
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
-
 app.listen(port, () => {
   console.log(`Movie Master Pro app listening on port ${port}`);
 });
